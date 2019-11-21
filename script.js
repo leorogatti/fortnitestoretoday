7a288736b822cddbf5f13c9e701ad3d8f25cf194
Leonardo Rogatti <leorrogatti@gmail.com>
2019-11-20 22:13:02 -0300

    Adicionada a contagem regressiva da loja.

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ axios
         divitems.appendChild(img)
     })
 })
+
+var updateTime= setInterval(function() {
+    var date= new Date
+    
+    var hour= 23 - date.getUTCHours()
+    var minute = 59 - date.getUTCMinutes()
+    var seconds = 60 - date.getUTCSeconds()
+
+    if (hour < 10) {
+        hour = '0' + hour
+    }
+    
+    if (minute < 10) {
+        minute = '0' + minute
+    }
+
+    if (seconds < 10) {
+        seconds = '0' + seconds  
+    }
+
+    var timetoUpdate = hour + ':' + minute + ':' + seconds
+
+    if (timetoUpdate === '00:00:00'){
+        clearInterval(updateTime)
+        setInterval(function() {
+            location.reload()
+        }, 5000);
+
+    }
+
+    document.getElementById('TimeToUpdate').innerHTML = timetoUpdate
+}, 1000);
+
